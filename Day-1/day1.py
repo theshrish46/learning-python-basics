481b970409a7d9a21829723a7dacb42e032ee1bd
theshrish46 <theshrish46@gmail.com>
2022-11-14 21:47:10 +0530

    added readme file

---
+++ Day-1/day1.py
@@ -0,0 +1,14 @@
+# Variables Python
+# Data types in Python
+# Operations in Python
+# Python tokens : Keywords, Identifiers, Literals & Operators
+import math
+import os
+import random
+import re
+import sys
+
+
+n = int(input().strip())
+print(n)
+
